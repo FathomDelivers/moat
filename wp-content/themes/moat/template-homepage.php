4362414bf59bf668ce4d0f8dd581faca6b31ad70
Gary Bunofsky <garybunofsky@gmail.com>
2015-04-16 20:32:21 -0400

    Added main-banner.php & _main-banner.less.

--- wp-content/themes/moat/template-homepage.php
+++ wp-content/themes/moat/template-homepage.php
@@ -3,6 +3,7 @@
  * Template Name: Homepage
  */
 ?>
+<?php get_template_part('templates/main-banner', 'page'); ?>
 <div class="col-md-8 col-md-offset-2"> 
 <?php while (have_posts()) : the_post(); ?>
   <?php get_template_part('templates/content', 'page'); ?>
---
+++ wp-content/themes/moat/templates/main-banner.php
@@ -0,0 +1,7 @@
+<div class="main-banner">
+	<div class="container-fluid">
+		<div class="col-md-12">
+		<h1>Advance your career.</h1>
+		</div> 
+	</div>
+</div>
