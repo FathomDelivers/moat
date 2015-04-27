051804ae2ac6dcf52ca51c8f9f3f68c1de26428e
Gary Bunofsky <garybunofsky@gmail.com>
2015-04-27 13:11:36 -0400

    Added 'Featured Image' functionality.

--- wp-content/themes/moat/templates/header.php
+++ wp-content/themes/moat/templates/header.php
@@ -24,4 +24,16 @@
       ?>
     </nav>
   </div>
+  <?php
+    if (has_post_thumbnail()) { //if a thumbnail has been set
+    $imgID = get_post_thumbnail_id($post->ID); //get the id of the featured image
+    $featuredImage = wp_get_attachment_image_src($imgID, 'full' );//get the url of the featured image (returns an array)
+    $imgURL = $featuredImage[0]; //get the url of the image out of the array
+  ?>
+  <style type="text/css">
+    .main-banner {border:none; background-image: url(<?php echo $imgURL ?>); } 
+  </style>
+  <?php
+    }//end if
+  ?>
 </header>
