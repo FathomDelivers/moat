f2a6939c75cdc6e27a0941561e9dd3a281165061
Gary Bunofsky <garybunofsky@gmail.com>
2015-06-02 08:54:59 -0400

    added inline styles for gradient – will be removing.

--- wp-content/themes/moat/template-single-purpose.php
+++ wp-content/themes/moat/template-single-purpose.php
@@ -11,3 +11,14 @@
 		</div>
 	</div>
 </div>
+<style type="text/css">
+	html { 
+		background-color:
+			background: -webkit-linear-gradient(90deg, #002c50 10%, #003865 90%); /* Chrome 10+, Saf5.1+ */
+  			background:    -moz-linear-gradient(90deg, #002c50 10%, #003865 90%); /* FF3.6+ */
+			background:     -ms-linear-gradient(90deg, #002c50 10%, #003865 90%); /* IE10 */
+			background:      -o-linear-gradient(90deg, #002c50 10%, #003865 90%); /* Opera 11.10+ */
+			background:         linear-gradient(90deg, #002c50 10%, #003865 90%); /* W3C */
+	 }
+	.navbar-default { background-color: transparent; }
+</style>
