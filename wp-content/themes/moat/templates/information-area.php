2a5a8669d94ef1445a7f5ceb4bd19bd429ac9460
Gary Bunofsky <garybunofsky@gmail.com>
2015-06-02 12:33:52 -0400

    included information-area.

---
+++ wp-content/themes/moat/templates/information-area.php
@@ -0,0 +1,14 @@
+<div class="information-area">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-4 col-md-offset-2">
+				<h3>What we have to offer.</h3>
+				<p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore.</p>
+			</div>
+			<div class="col-md-4">
+				<h3>A few other features.</h3>
+				<p>Enim ad minima veniam, quis nostrum exercitationem, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam.</p>
+			</div>
+		</div>
+	</div>
+</div>
