d5fce7393a38ed1dde944cd9b494375bd092a721
Gary Bunofsky <garybunofsky@gmail.com>
2015-06-02 12:36:43 -0400

    made form flush with main-banner. changed color.

--- wp-content/themes/moat/templates/main-banner.php
+++ wp-content/themes/moat/templates/main-banner.php
@@ -1,13 +1,11 @@
 <div class="main-banner">
-	<div class="container">
-		<div class="row">
-			<div class="col-md-8">
-				<h1><?php echo get_post_meta($post->ID, 'main-banner-heading', true); ?></h1>
-				<p class="lead"><?php echo get_post_meta($post->ID, 'main-banner-lead', true); ?></p>
-			</div>
-			<div class="col-md-4">
-				<?php get_template_part('templates/salesforce-form', 'page'); ?>
-			</div>
+	<div class="container-fluid">
+		<div class="col-sm-6 col-sm-offset-1">
+			<h1><?php echo get_post_meta($post->ID, 'main-banner-heading', true); ?></h1>
+			<p class="lead"><?php echo get_post_meta($post->ID, 'main-banner-lead', true); ?></p>
+		</div>
+		<div class="col-sm-4 background">
+			<?php get_template_part('templates/salesforce-form', 'page'); ?>
 		</div>
 	</div>
 </div>
