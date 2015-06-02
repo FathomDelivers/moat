606d5ddac7f35ac02267b31758ad489f5e39d96e
Gary Bunofsky <garybunofsky@gmail.com>
2015-06-02 08:57:14 -0400

    menu button now follows logo.

--- wp-content/themes/moat/templates/header.php
+++ wp-content/themes/moat/templates/header.php
@@ -3,19 +3,12 @@
 <header class="banner navbar navbar-default navbar-static-top" role="banner">
   <div class="container">
     <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
-        <span class="sr-only"><?= __('Toggle navigation', 'sage'); ?></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
       <?php if ( get_theme_mod( 'themeslug_logo' ) ) : ?>
       <div class='site-logo'>
           <a class="navbar-brand" href='<?= esc_url(home_url('/' )); ?>' title='<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>' rel='home'><img src='<?php echo esc_url( get_theme_mod( 'themeslug_logo' ) ); ?>' alt='<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>'></a>
       </div>
       <?php endif; ?>
     </div>
-
     <nav class="collapse navbar-collapse" role="navigation">
       <?php
       if (has_nav_menu('primary_navigation')) :
@@ -23,6 +16,12 @@
       endif;
       ?>
     </nav>
+    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+        <span class="sr-only"><?= __('Toggle navigation', 'sage'); ?></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+    </button>
   </div>
   <?php
     if (has_post_thumbnail()) { //if a thumbnail has been set
