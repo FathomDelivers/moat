4c128350522862c30de3623f2d21361c7cc0791d
Gary Bunofsky <garybunofsky@gmail.com>
2015-06-02 12:35:37 -0400

    removed contact-band from homepage template. removed program-area from homepage template.

--- wp-content/themes/moat/template-homepage.php
+++ wp-content/themes/moat/template-homepage.php
@@ -7,8 +7,6 @@
 
 <?php while (have_posts()) : the_post(); ?>
   <?php get_template_part('templates/content', 'page'); ?>
-  <?php get_template_part('templates/feature-area', 'page'); ?>
-  <?php get_template_part('templates/contact-band', 'page'); ?>
-  <?php get_template_part('templates/program-area', 'page'); ?>
+  <?php get_template_part('templates/information-area', 'page'); ?>
 <?php endwhile; ?>
 
