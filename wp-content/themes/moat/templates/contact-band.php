8bd092d9a5c4110461bd5c00f89231e4c08164ab
Gary Bunofsky <garybunofsky@gmail.com>
2015-06-15 12:55:45 -0400

    Reduced the length of copy.

--- wp-content/themes/moat/templates/contact-band.php
+++ wp-content/themes/moat/templates/contact-band.php
@@ -4,8 +4,10 @@
 			<img src="<?php echo get_template_directory_uri(); ?>/assets/images/phone.png" />
 		</div>
 		<div class="col-md-6 col-md-offset-1">
-			<h2><?php echo get_post_meta($post->ID, 'contact-band-heading', true); ?></h2>
-			<p>Ullamcorper, duo et soleat vocent, definiebas interesset mea cu. Ea forensibus consetetur vix, sit modo fugit. Indoctum salutandi his ea, vim esse autem consulatu ex.</p>
+			<div class="col-sm-12">
+				<h2><?php echo get_post_meta($post->ID, 'contact-band-heading', true); ?></h2>
+				<p>Ullamcorper, duo et soleat vocent, definiebas interesset mea cuvix. Indoctum salutandi his ea, esse autem consulatu.</p>
+			</div>
 			<div class="col-sm-6">
 				<a href="tel:<?php echo get_post_meta($post->ID, 'contact-band-button-one', true); ?>" class="btn-sm-o"><i class="fa fa-phone">&nbsp;</i><?php echo get_post_meta($post->ID, 'contact-band-button-one', true); ?></a>
 			</div>
