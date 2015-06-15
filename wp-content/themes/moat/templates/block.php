3a35f00324c034a49d1771c6e8a58298e1decde7
Gary Bunofsky <garybunofsky@gmail.com>
2015-06-15 12:39:10 -0400

    Included block section.

---
+++ wp-content/themes/moat/templates/block.php
@@ -0,0 +1,10 @@
+<div class="block">
+	<div class="container">
+		<div class="col-sm-8 col-sm-offset-2">
+			<h3><?php echo get_post_meta($post->ID, 'block-heading', true); ?></h3>
+			<div class="col-sm-8 col-sm-offset-2">
+				<a href="<?php echo get_post_meta($post->ID, 'block-button', true); ?>" class="btn-sm"><?php echo get_post_meta($post->ID, 'block-button', true); ?></a>
+			</div>
+		</div>
+	</div>
+</div>
