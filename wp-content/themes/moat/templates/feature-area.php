85c54ea5d541acbc42e9cf140ee73fa72453c539
Gary Bunofsky <garybunofsky@gmail.com>
2015-06-15 12:46:06 -0400

    Restructured feature-area.

--- wp-content/themes/moat/templates/feature-area.php
+++ wp-content/themes/moat/templates/feature-area.php
@@ -2,19 +2,37 @@
 	<div class="container">
 		<div class="row center">
 			<div class="col-md-4">
-				<i class="fa fa-graduation-cap">&nbsp;</i>
-				<h3>Feature One</h3>
-				<p>Vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores ratione.</p> 
+				<section>
+					<div class="upper">
+						<img src="<?php echo get_template_directory_uri(); ?>/assets/images/meeting.jpg" />
+					</div>
+					<div class="lower">
+						<h3>Featured Course</h3>
+						<p>Vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores ratione.</p> 
+					</div>
+				</section>
 			</div>
 			<div class="col-md-4">
-				<i class="fa fa-group">&nbsp;</i>
-				<h3>Feature Two</h3>
-				<p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore.</p>
+				<section>
+					<div class="upper">
+						<img src="<?php echo get_template_directory_uri(); ?>/assets/images/computer.jpg" />
+					</div>
+					<div class="lower">
+						<h3>Featured Course</h3>
+						<p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore.</p>
+					</div>
+				</section>
 			</div>
 			<div class="col-md-4">
-				<i class="fa fa-book">&nbsp;</i>
-				<h3>Feature Three</h3>
-				<p>Enim ad minima veniam, quis nostrum exercitationem, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam.</p>
+				<section>
+					<div class="upper">
+						<img src="<?php echo get_template_directory_uri(); ?>/assets/images/meeting.jpg" />
+					</div>
+					<div class="lower">
+						<h3>Featured Course</h3>
+						<p>Enim ad minima veniam, quis nostrum exercitationem, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam.</p>
+					</div>
+				</section>
 			</div>
 		</div>
 	</div>
