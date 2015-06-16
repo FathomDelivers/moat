cb1f6ab78f58d1c23de62689d79a66fdfc5a6fa3
Gary Bunofsky <garybunofsky@gmail.com>
2015-06-16 16:52:30 -0400

    Added privacy policy.

--- wp-content/themes/moat/templates/footer.php
+++ wp-content/themes/moat/templates/footer.php
@@ -2,7 +2,7 @@
   <div class="container">
   	<div class="col-sm-8 col-sm-offset-2">
     	<?php dynamic_sidebar('sidebar-footer'); ?>
-    	<p>&copy; Copyright <?php echo date('Y') ?> <?php echo get_post_meta($post->ID, 'footer-name', true); ?></p>
+    	<p>&copy; Copyright <?php echo date('Y') ?> <?php echo get_post_meta($post->ID, 'footer-name', true); ?> | <a href="privacy-policy">Privacy Policy</a></p>
   	</div>
   </div>
 </footer>
