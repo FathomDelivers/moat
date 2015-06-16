ed65f8eb98451084fc2829209cac801320f906b7
Gary Bunofsky <garybunofsky@gmail.com>
2015-06-16 16:52:44 -0400

    Switched location of form.

--- wp-content/themes/moat/templates/main-banner.php
+++ wp-content/themes/moat/templates/main-banner.php
@@ -1,11 +1,11 @@
 <div class="main-banner">
 	<div class="container-fluid">
-		<div class="col-sm-6 col-sm-offset-1">
+		<div class="col-sm-4 col-sm-offset-1">
+			<?php get_template_part('templates/salesforce-form', 'page'); ?>
+		</div>
+		<div class="col-sm-6">
 			<h1><?php echo get_post_meta($post->ID, 'main-banner-heading', true); ?></h1>
 			<p class="lead"><?php echo get_post_meta($post->ID, 'main-banner-lead', true); ?></p>
-		</div>
-		<div class="col-sm-4 background">
-			<?php get_template_part('templates/salesforce-form', 'page'); ?>
 		</div>
 	</div>
 </div>
