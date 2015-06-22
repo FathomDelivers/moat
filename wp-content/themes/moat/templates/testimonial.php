7e07ef544ad2ddd30d5fe3c2b0841de6a7d40d4a
Gary Bunofsky <garybunofsky@gmail.com>
2015-06-22 15:39:55 -0400

    Added testimonial section.

--- wp-content/themes/moat/templates/testimonial.php
+++ wp-content/themes/moat/templates/testimonial.php
@@ -1,9 +1,29 @@
 <div class="container-fluid testimonial">
-	<div class="col-md-1 col-md-offset-4">
-		<img src="<?php echo get_template_directory_uri(); ?>/assets/images/testimonial.jpg" alt="">
-	</div>
-	<div class="col-md-4 testimonial-content">
-		<p><strong>"</strong>Enim ad minima veniam, quis nostrum exercitationem, sed quia consequuntur magni dolores.Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur.<strong>"</strong></p>
-		<span>Roberta Flack</span>
+	<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+		<div class="col-md-6 col-md-offset-3">
+		  <div class="carousel-inner">
+		    <div class="item active">
+		        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/testimonial-1.jpg" alt="">
+		        <p><strong>"</strong>Enim ad minima veniam, quis nostrum exercitationem, sed quia consequuntur magni dolores.Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur.<strong>"</strong></p>
+				<span>Roberta F.</span>
+		    </div>
+		    <div class="item">
+		        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/testimonial-2.jpg" alt="">
+		        <p><strong>"</strong>Enim ad minima veniam, quis nostrum exercitationem, sed quia consequuntur magni dolores.Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur.<strong>"</strong></p>
+				<span>Stewart P.</span>
+		    </div>
+		    <div class="item">
+		        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/testimonial-3.jpg" alt="">
+		        <p><strong>"</strong>Enim ad minima veniam, quis nostrum exercitationem, sed quia consequuntur magni dolores.Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur.<strong>"</strong></p>
+				<span>Kristem L.</span>
+		    </div>
+		  </div>
+		</div>
+		<a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+		  <i class="fa fa-angle-left"></i>
+		</a>
+		<a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+		  <i class="fa fa-angle-right"></i>
+		</a>
 	</div>
 </div>
