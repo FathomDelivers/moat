1399cd4cc9a3155fb893fcc4fdaf4fd3d0128a07
Gary Bunofsky <garybunofsky@gmail.com>
2015-06-22 15:40:32 -0400

    Added icon-area.

---
+++ wp-content/themes/moat/templates/icon-area.php
@@ -0,0 +1,27 @@
+<div class="icon-area">
+	<div class="container">
+		<div class="row center">
+			<div class="col-md-4">
+				<div class="circle">
+					<i class="fa fa-laptop">&nbsp;</i>
+				</div>
+				<h4>What we have to offer.</h4>
+				<p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore.</p>
+			</div>
+			<div class="col-md-4">
+				<div class="circle">
+					<i class="fa fa-graduation-cap">&nbsp;</i>
+				</div>
+				<h4>A few other features.</h4>
+				<p>Enim ad minima veniam, quis nostrum exercitationem, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam.</p>
+			</div>
+			<div class="col-md-4">
+				<div class="circle">
+					<i class="fa fa-university">&nbsp;</i>
+				</div>
+				<h4>A few other features.</h4>
+				<p>Enim ad minima veniam, quis nostrum exercitationem, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam.</p>
+			</div>
+		</div>
+	</div>
+</div>
