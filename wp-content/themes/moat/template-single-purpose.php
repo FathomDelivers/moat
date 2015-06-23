6ee4085c28616590390a453af0be33a01830712a
Gary Bunofsky <garybunofsky@gmail.com>
2015-06-23 11:53:35 -0400

    Removed inline styles. Added styles to pages.less

--- wp-content/themes/moat/template-single-purpose.php
+++ wp-content/themes/moat/template-single-purpose.php
@@ -4,21 +4,8 @@
  */
 ?>
 
-<div class="container">
-	<div class="row">
+<div class="container-fluid">
 		<div class="col-md-4 col-md-offset-4">
 			<?php get_template_part('templates/salesforce-form', 'page'); ?>
 		</div>
-	</div>
 </div>
-<style type="text/css">
-	html { 
-		background-color:
-			background: -webkit-linear-gradient(90deg, #002c50 10%, #003865 90%); /* Chrome 10+, Saf5.1+ */
-  			background:    -moz-linear-gradient(90deg, #002c50 10%, #003865 90%); /* FF3.6+ */
-			background:     -ms-linear-gradient(90deg, #002c50 10%, #003865 90%); /* IE10 */
-			background:      -o-linear-gradient(90deg, #002c50 10%, #003865 90%); /* Opera 11.10+ */
-			background:         linear-gradient(90deg, #002c50 10%, #003865 90%); /* W3C */
-	 }
-	.navbar-default { background-color: transparent; }
-</style>
