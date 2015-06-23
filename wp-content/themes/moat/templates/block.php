44e046275946e600884d7f058861209b6cfda0f0
Gary Bunofsky <garybunofsky@gmail.com>
2015-06-23 12:00:04 -0400

    Revised columns. Added link theme options.

--- wp-content/themes/moat/templates/block.php
+++ wp-content/themes/moat/templates/block.php
@@ -2,8 +2,8 @@
 	<div class="container">
 		<div class="col-sm-8 col-sm-offset-2">
 			<h3><?php echo get_post_meta($post->ID, 'block-heading', true); ?></h3>
-			<div class="col-sm-8 col-sm-offset-2">
-				<a href="<?php echo get_post_meta($post->ID, 'block-button', true); ?>" class="btn-sm"><?php echo get_post_meta($post->ID, 'block-button', true); ?></a>
+			<div class="col-sm-4 col-sm-offset-4">
+				<a href="<?php echo get_post_meta($post->ID, 'block-button-link', true); ?>" class="btn-sm"><?php echo get_post_meta($post->ID, 'block-button', true); ?></a>
 			</div>
 		</div>
 	</div>
