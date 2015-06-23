062e4fd866ce919c64f2ee527d978c21ac51310a
Gary Bunofsky <garybunofsky@gmail.com>
2015-06-23 11:54:33 -0400

    Revised copy.

--- wp-content/themes/moat/templates/information-area.php
+++ wp-content/themes/moat/templates/information-area.php
@@ -2,12 +2,12 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-md-4 col-md-offset-2">
-				<h3>What we have to offer.</h3>
-				<p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore.</p>
+				<h3>One of our features.</h3>
+				<p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt.</p>
 			</div>
 			<div class="col-md-4">
-				<h3>A few other features.</h3>
-				<p>Enim ad minima veniam, quis nostrum exercitationem, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam.</p>
+				<h3>Another feature.</h3>
+				<p>Congue vocent definitionem per ut, ea mea quas dicam. No saperet euripidis has. In omnium neglegentur complectitur mel.</p>
 			</div>
 		</div>
 	</div>
