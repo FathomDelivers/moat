68938c9a6356be4d4fea6d876d6df4e4548201a7
Gary Bunofsky <garybunofsky@gmail.com>
2015-06-23 11:55:06 -0400

    Edited feature-area.php copy.

--- wp-content/themes/moat/templates/feature-area.php
+++ wp-content/themes/moat/templates/feature-area.php
@@ -7,8 +7,8 @@
 						<img src="<?php echo get_template_directory_uri(); ?>/assets/images/meeting.jpg" />
 					</div>
 					<div class="lower">
-						<h3>Featured Course</h3>
-						<p>Vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores ratione.</p> 
+						<h3>Courses</h3>
+						<p>Minim quando repudiare ne ius, dicant gubergren ei eos. Nostro officiis ex sed, option neglegentur vis ad. Sapientem intellegebat neccu. Consectetuer duo dolor utinam diceret sit quas doctus.</p> 
 					</div>
 				</section>
 			</div>
@@ -18,8 +18,8 @@
 						<img src="<?php echo get_template_directory_uri(); ?>/assets/images/computer.jpg" />
 					</div>
 					<div class="lower">
-						<h3>Featured Course</h3>
-						<p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore.</p>
+						<h3>Graduation</h3>
+						<p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt. Sensibus quem referrentur causae intellegebat.</p>
 					</div>
 				</section>
 			</div>
@@ -29,8 +29,8 @@
 						<img src="<?php echo get_template_directory_uri(); ?>/assets/images/meeting.jpg" />
 					</div>
 					<div class="lower">
-						<h3>Featured Course</h3>
-						<p>Enim ad minima veniam, quis nostrum exercitationem, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam.</p>
+						<h3>Campus Life</h3>
+						<p>Oporteat elaboraret quo ei. Soleat debitis evertitur et ius, falli omittam eu mea. Sit lucilius recusabo salutatus et, vim erat fabellas. Sed an nostro labores consul scaevola numquam vim sapien.</p> 
 					</div>
 				</section>
 			</div>
