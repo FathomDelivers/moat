b9771379d97a80c7dfa91de7c1a34ecf012dba3c
Gary Bunofsky <garybunofsky@gmail.com>
2015-06-26 16:18:03 -0400

    Revised area names in preview template.

--- wp-content/themes/moat/template-preview.php
+++ wp-content/themes/moat/template-preview.php
@@ -8,11 +8,11 @@
 <?php while (have_posts()) : the_post(); ?>
   <?php get_template_part('templates/content', 'page'); ?>
   <?php get_template_part('templates/icon-area', 'page'); ?>
-  <?php get_template_part('templates/contact-band', 'page'); ?>
+  <?php get_template_part('templates/contact-area', 'page'); ?>
   <?php get_template_part('templates/information-area', 'page'); ?>
   <?php get_template_part('templates/feature-area', 'page'); ?>
   <?php get_template_part('templates/testimonial', 'page'); ?>
-  <?php get_template_part('templates/block', 'page'); ?>
+  <?php get_template_part('templates/block-area', 'page'); ?>
 <?php endwhile; ?>
 <?php
     get_template_part('templates/footer');
