fab4fa31837ec7e3020d1d10a8af4d520c2bad0e
Gary Bunofsky <garybunofsky@gmail.com>
2015-06-26 16:18:50 -0400

    Increased height of lower. Included custom theme options.

--- wp-content/themes/moat/templates/feature-area.php
+++ wp-content/themes/moat/templates/feature-area.php
@@ -1,39 +1,59 @@
+<?php
+
+// Should feature area be displayed?
+if ( get_field('feature_area_on') ) { $featureAreaOn = get_field('feature_area_on'); } 
+else { $featureAreaOn = '<h2>Please add a header</h2><br /><p>Please add some paragraph content</p>'; }
+
+// Content
+if ( get_field('feature_area') ) { $featureAreaContent = get_field('feature_area'); 
+$value = get_field( "text_field" );}
+else { $featureAreaContent = '<h2>Please add a header</h2><br /><p>Please add some paragraph content</p>'; }
+
+?>
+
+<?php if ($featureAreaOn == 'On') {
+	// Set fields as variables
+	$feature_area_content_column_1 = get_field( "feature_area_content_column_1" );
+	$feature_area_image_1    = get_field( "feature_area_image_1" );
+	$feature_area_content_column_2 = get_field( "feature_area_content_column_2" );
+	$feature_area_image_2    = get_field( "feature_area_image_2" );
+	$feature_area_content_column_3 = get_field( "feature_area_content_column_3" );
+	$feature_area_image_3    = get_field( "feature_area_image_3" );
+    ?>
 <div class="feature-area">
 	<div class="container">
 		<div class="row center">
 			<div class="col-md-4">
 				<section>
 					<div class="upper">
-						<img src="<?php echo get_template_directory_uri(); ?>/assets/images/meeting.jpg" />
+						<img src="<?php echo $feature_area_image_1['url']; ?>" alt="<?php echo $feature_area_image_1['alt']; ?>" />
 					</div>
 					<div class="lower">
-						<h3>Courses</h3>
-						<p>Minim quando repudiare ne ius, dicant gubergren ei eos. Nostro officiis ex sed, option neglegentur vis ad. Sapientem intellegebat neccu. Consectetuer duo dolor utinam diceret sit quas doctus.</p> 
+						<?php echo $feature_area_content_column_1; ?>
 					</div>
 				</section>
 			</div>
 			<div class="col-md-4">
 				<section>
 					<div class="upper">
-						<img src="<?php echo get_template_directory_uri(); ?>/assets/images/computer.jpg" />
+						<img src="<?php echo $feature_area_image_2['url']; ?>" alt="<?php echo $feature_area_image_2['alt']; ?>" />
 					</div>
 					<div class="lower">
-						<h3>Graduation</h3>
-						<p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt. Sensibus quem referrentur causae intellegebat.</p>
+						<?php echo $feature_area_content_column_2; ?>
 					</div>
 				</section>
 			</div>
 			<div class="col-md-4">
 				<section>
 					<div class="upper">
-						<img src="<?php echo get_template_directory_uri(); ?>/assets/images/meeting.jpg" />
+						<img src="<?php echo $feature_area_image_3['url']; ?>" alt="<?php echo $feature_area_image_3['alt']; ?>" />
 					</div>
 					<div class="lower">
-						<h3>Campus Life</h3>
-						<p>Oporteat elaboraret quo ei. Soleat debitis evertitur et ius, falli omittam eu mea. Sit lucilius recusabo salutatus et, vim erat fabellas. Sed an nostro labores consul scaevola numquam vim sapien.</p> 
+						<?php echo $feature_area_content_column_3; ?>
 					</div>
 				</section>
 			</div>
 		</div>
 	</div>
 </div>
+<?php } else { /* Nothing to show here.. */ } ?>
