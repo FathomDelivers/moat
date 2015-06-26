57cadeb639bce1810cd1c84c69c2b19e970ffbe1
Gary Bunofsky <garybunofsky@gmail.com>
2015-06-26 12:03:47 -0400

    Added custom theme options for contact-band.

--- README.md
+++ README.md
@@ -1,29 +1,41 @@
 #Module Oriented Active Theme
 This repository gives everyone the ability to deploy a landing page for marketing campaigns. Essentially reducing development time.
 
+##Table of Contents
+1. Installation
+2. Adding a logo
+3.
+4.
+
+
+###Installation
+
+###Adding a logo
+Once logged into the Wordpress dashboard hover over **Appearence** located on the left sidebar. Then click on **Customize**. Click on **Logo** menu item dropdown. Then click on **Change Image**, 
+
+
+##What is this theme built on?
+This code is build using [WordPress](http://wordpress.org) along with an instance of [Sage](https://github.com/roots/sage). Styles compile with [Sass](http://sass-lang.com/) using [Gulp](http://gulpjs.com/). The form holds values with [Garlic](http://garlicjs.org/).
+
 ##Feature Timeline
-#####Sprint One *(July 7th 2015)*
+####Sprint One *(July 7th 2015)*
 - All custom theme options 
 - Top phone number
 - Ability to change icons
 - Ability to add/remove block elements
 
-#####Sprint Two *(July 14th 2015)*
+####Sprint Two *(July 14th 2015)*
 - Ability to change images 
 - Ability to change colors
 - Integration with Pass.js
 - Code additional design elements
 
-#####Sprint Three *(July 21st 2015)*
+####Sprint Three *(July 21st 2015)*
 - Form expression copy 
 - Compose thorough documentation
 - Customizable main-banner
 - Variations of main-banner
 
-#####Sprint Four *(July 28th 2015)*
+####Sprint Four *(July 28th 2015)*
 - User Testing
 - Iterate
-
-##What is this theme built on?
-This code is build using [WordPress](http://wordpress.org) along with an instance of [Sage](https://github.com/roots/sage). Styles compile with [Sass](http://sass-lang.com/) using [Gulp](http://gulpjs.com/). The form holds values with [Garlic](http://garlicjs.org/).
-
--- wp-content/themes/moat/templates/contact-band.php
+++ wp-content/themes/moat/templates/contact-band.php
@@ -1,19 +1,33 @@
-<div class="container-fluid contact-band">
-	<div class="row">
-		<div class="col-md-3 col-md-offset-1">
-			<img src="<?php echo get_template_directory_uri(); ?>/assets/images/phone.png" />
-		</div>
-		<div class="col-md-6 col-md-offset-1">
-			<div class="col-sm-12">
-				<h2><?php echo get_post_meta($post->ID, 'contact-band-heading', true); ?></h2>
-				<p>Ullamcorper, duo et soleat vocent, definiebas interesset mea cuvix. Indoctum salutandi his ea, esse autem consulatu.</p>
-			</div>
-			<div class="col-sm-4">
-				<a href="tel:<?php echo get_post_meta($post->ID, 'contact-band-button-one-link', true); ?>" class="btn-sm-o white"><i class="fa fa-phone">&nbsp;</i> <?php echo get_post_meta($post->ID, 'contact-band-button-one', true); ?></a>
-			</div>
-			<div class="col-sm-4">
-				<a href="<?php echo get_post_meta($post->ID, 'contact-band-button-two-link', true); ?>" class="btn-sm-o"><i class="fa fa-book">&nbsp;</i> <?php echo get_post_meta($post->ID, 'contact-band-button-two', true); ?></a>
-			</div>
-		</div>
-	</div>
-</div>
+<?php
+
+// Should contact band be displayed?
+if ( get_field('contact_band_on') ) { $contactBandOn = get_field('contact_band_on'); } 
+else { $contactBandOn = 'Default Value if not entered?'; }
+
+// Content
+if ( get_field('contact_band') ) { $contactBandContent = get_field('contact_band'); }
+else { $contactBandContent = 'Default Value if not entered?'; }
+
+?>
+
+<?php if ($contactBandOn == 'On') {
+    ?>
+    <div class="container-fluid contact-band">
+        <div class="row">
+            <div class="col-md-3 col-md-offset-1">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/phone.png" />
+            </div>
+            <div class="col-md-6 col-md-offset-1">
+                <div class="col-sm-12">
+                    <p><?php echo $contactBandContent; ?></p>
+                </div>
+                <div class="col-sm-4">
+                    <a href="<?php get_field('contact_band_button_1_link')?>" class="btn-sm-o white"><i class="fa fa-phone">&nbsp;</i><?php get_field('contact_band_button_1')?></a>
+                </div>
+                <div class="col-sm-4">
+                    <a href="<?php echo get_post_meta($post->ID, 'contact-band-button-two-link', true); ?>" class="btn-sm-o"><i class="fa fa-book">&nbsp;</i> <?php echo get_post_meta($post->ID, 'contact-band-button-two', true); ?></a>
+                </div>
+            </div>
+        </div>
+    </div>
+<?php } else { /* Nothing to show here.. */ } ?>
