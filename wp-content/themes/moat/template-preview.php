29714e8e5bef0af76154717dddb33640c4964604
Gary Bunofsky <garybunofsky@gmail.com>
2015-07-02 11:41:42 -0400

    Included announcement-area.

--- wp-content/themes/moat/template-preview.php
+++ wp-content/themes/moat/template-preview.php
@@ -6,6 +6,7 @@
 <?php get_template_part('templates/main-banner', 'page'); ?>
 
 <?php while (have_posts()) : the_post(); ?>
+	<?php get_template_part('templates/announcement-area', 'page'); ?>
   <?php get_template_part('templates/content', 'page'); ?>
   <?php get_template_part('templates/icon-area', 'page'); ?>
   <?php get_template_part('templates/contact-area', 'page'); ?>
