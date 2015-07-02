5b5e928fffec1297bc6e5a6650a98da8bf4c15a6
Gary Bunofsky <garybunofsky@gmail.com>
2015-07-02 11:43:02 -0400

    Forgot to add announcement-area.

---
+++ wp-content/themes/moat/templates/announcement-area.php
@@ -0,0 +1,17 @@
+<div class="announcement-area">
+<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner col-xs-12">
+    <div class="item active">
+      <a href="#"><p><strong>Learn about our courses:</strong> View the gated content <span>→</span></p></a>
+    </div>
+    <div class="item">
+    	<a href="#"><p><strong>New to applying?</strong> Get started by completing our registration <span>→</span></p></a>
+    </div>
+  </div>
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="1" ></li>
+  </ol>
+</div>
+</div>
