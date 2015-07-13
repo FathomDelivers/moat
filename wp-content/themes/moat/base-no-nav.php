59a93fce4d230831a1a75705322aee621297d03c
Gary Bunofsky <garybunofsky@gmail.com>
2015-07-13 12:03:53 -0400

    Created base-no-nav.php

---
+++ wp-content/themes/moat/base-no-nav.php
@@ -0,0 +1,30 @@
+<?php
+
+use Roots\Sage\Config;
+use Roots\Sage\Wrapper;
+
+?>
+
+<?php get_template_part('templates/head'); ?>
+  <body <?php body_class(); ?>>
+    <!--[if lt IE 9]>
+      <div class="alert alert-warning">
+        <?php _e('You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.', 'sage'); ?>
+      </div>
+    <![endif]-->
+
+      <div class="content">
+          <?php include Wrapper\template_path(); ?>
+        <?php if (Config\display_sidebar()) : ?>
+          <aside class="sidebar" role="complementary">
+            <?php include Wrapper\sidebar_path(); ?>
+          </aside><!-- /.sidebar -->
+        <?php endif; ?>
+      </div><!-- /.content -->
+
+    <!--php
+      get_template_part('templates/footer');
+      wp_footer();
+    ?-->
+  </body>
+</html>
